ff83289636fc15ce90cb4402d6249fe0ff6cc382
marco <marco@MFAB>
2017-12-02 20:31:56 -0600

    saql date

--- src/Classes/Reservation.java
+++ src/Classes/Reservation.java
@@ -5,6 +5,9 @@
  */
 package Classes;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author Steve
@@ -12,8 +15,8 @@ package Classes;
 public class Reservation {
 
     private int Roomnumber;
-    private java.sql.Date EntryDate;
-    private java.sql.Date ExitDate;
+    private String EntryDate;
+    private String ExitDate;
     private int NighstNumber;
     private String RoomType;
     private String RoomAmount;
@@ -21,10 +24,104 @@ public class Reservation {
     private String UserName;
     private String ChildrenAmount;
     private String AdultAmount;
-    
-    
-    
-    
-    
-    
+
+    public Reservation(int Roomnumber, String EntryDate, String ExitDate, int NighstNumber, String RoomType, String RoomAmount, int roomprice, String UserName, String ChildrenAmount, String AdultAmount) {
+        this.Roomnumber = Roomnumber;
+        this.EntryDate = EntryDate;
+        this.ExitDate = ExitDate;
+        this.NighstNumber = NighstNumber;
+        this.RoomType = RoomType;
+        this.RoomAmount = RoomAmount;
+        this.roomprice = roomprice;
+        this.UserName = UserName;
+        this.ChildrenAmount = ChildrenAmount;
+        this.AdultAmount = AdultAmount;
+    }
+
+    public int getRoomnumber() {
+        return Roomnumber;
+    }
+
+    public void setRoomnumber(int Roomnumber) {
+        this.Roomnumber = Roomnumber;
+    }
+
+    public String getEntryDate() {
+        return EntryDate;
+    }
+
+    public void setEntryDate(String EntryDate) {
+        this.EntryDate = EntryDate;
+    }
+
+    public String getExitDate() {
+        return ExitDate;
+    }
+
+    public void setExitDate(String ExitDate) {
+        this.ExitDate = ExitDate;
+    }
+
+    public int getNighstNumber() {
+        return NighstNumber;
+    }
+
+    public void setNighstNumber(int NighstNumber) {
+        this.NighstNumber = NighstNumber;
+    }
+
+    public String getRoomType() {
+        return RoomType;
+    }
+
+    public void setRoomType(String RoomType) {
+        this.RoomType = RoomType;
+    }
+
+    public String getRoomAmount() {
+        return RoomAmount;
+    }
+
+    public void setRoomAmount(String RoomAmount) {
+        this.RoomAmount = RoomAmount;
+    }
+
+    public int getRoomprice() {
+        return roomprice;
+    }
+
+    public void setRoomprice(int roomprice) {
+        this.roomprice = roomprice;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String UserName) {
+        this.UserName = UserName;
+    }
+
+    public String getChildrenAmount() {
+        return ChildrenAmount;
+    }
+
+    public void setChildrenAmount(String ChildrenAmount) {
+        this.ChildrenAmount = ChildrenAmount;
+    }
+
+    public String getAdultAmount() {
+        return AdultAmount;
+    }
+
+    public void setAdultAmount(String AdultAmount) {
+        this.AdultAmount = AdultAmount;
+    }
+
+    public static String actualdate() {// static methow for get actully date for reservation day
+        Date actualdate = new Date();
+        SimpleDateFormat formatdate = new SimpleDateFormat("dd/MM/YYYY");
+        return formatdate.format(actualdate);
+    }
+
 }
