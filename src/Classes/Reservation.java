d7f8908ebf57865fdf9d3197ce62551980f3e9ce
marco <marco@MFAB>
2017-12-02 20:14:45 -0600

    saql date

--- src/Classes/Reservation.java
+++ src/Classes/Reservation.java
@@ -10,7 +10,17 @@ package Classes;
  * @author Steve
  */
 public class Reservation {
-    
+
+    private int Roomnumber;
+    private java.sql.Date EntryDate;
+    private java.sql.Date ExitDate;
+    private int NighstNumber;
+    private String RoomType;
+    private String RoomAmount;
+    private int roomprice;
+    private String UserName;
+    private String ChildrenAmount;
+    private String AdultAmount;
     
     
     
